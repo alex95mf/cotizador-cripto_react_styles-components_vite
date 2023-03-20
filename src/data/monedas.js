010338439562542a50bf1a3d8967ed16415b7787
ronalmar <ronalmar@espol.edu.ec>
2023-03-19 19:55:57 -0500

    Fixed error

--- src/data/monedas.js
+++ src/data/monedas.js
@@ -1,6 +1,6 @@
 const monedas = [
   {id: 'USD', nombre: 'Dólar de Estados Unidos'},
-  {id: 'MXN', nombre: 'Perso Mexicano'},
+  {id: 'MXN', nombre: 'Peso Mexicano'},
   {id: 'EUR', nombre: 'Euro'},
   {id: 'GBP', nombre: 'Libra Esterlina'}
 ]
